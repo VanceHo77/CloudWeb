eb4a73979e90cda580a5529fd494067ea9124a8c
vanceho <vanceho@intumit.com>
2016-12-11 00:32:00 +0800

    add header.component & css & images

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Router, Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: '/', pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
