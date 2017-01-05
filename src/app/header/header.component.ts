f60ed672db10de89e4568f394d8f62b2b4408750
vanceho <vanceho@intumit.com>
2017-01-05 20:04:10 +0800

    首頁-天氣快報: 可正常顯示dialog

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,42 @@
 import { HeaderWebmenuService } from './header-webmenu/header-webmenu.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Inject } from '@angular/core';
+
+
+import * as $ from 'jquery';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  providers:[HeaderWebmenuService]
+  styleUrls: ['./header.component.css'],
+  providers: [HeaderWebmenuService]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  elementRef: ElementRef;
 
-  ngOnInit() {
+  constructor( @Inject(ElementRef) elementRef: ElementRef) {
+    this.elementRef = elementRef;
   }
 
+  ngAfterViewInit() {
+    jQuery(this.elementRef.nativeElement)
+      .find('#dialog')
+      .dialog({
+        autoOpen: false,
+        modal: false,
+      });
+
+    $(".ui-icon-closethick").bind('click', function () {
+      $(".ui-dialog").toggle();
+    });
+  }
+
+  displayWeather(event: Event) {
+    event.preventDefault();
+    //因使用原API提供方式無法正常開啟Dialog，故自己設定顯示/隱藏
+    $(".ui-dialog").toggle();
+    let offset = $("#imgWeather").offset();
+    $('.ui-dialog')[0].setAttribute('style', 'position:absolute;top:' + (offset.top + 50) + 'px;left:' + offset.left + 'px;');
+  }
 }
