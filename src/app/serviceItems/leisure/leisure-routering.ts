e46f36ca270e1bccc95ba00cb8b04c24618ffa22
vanceho <vanceho@intumit.com>
2017-01-17 11:46:31 +0800

    第二頁呈現方式改用模板方式-3

--- src/app/serviceItems/leisure/leisure-routering.ts
+++ src/app/serviceItems/leisure/leisure-routering.ts
@@ -1,6 +1,5 @@
 import { SecondLayerComponent } from './../../core/template/second-layer.component';
 import { AccommodationComponent } from './accommodation/accommodation.component';
-import { LeisureComponent } from './leisure.component';
 import { NgModule } from '@angular/core';
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
